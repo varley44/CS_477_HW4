5d919d79f6c1ba67955f37bb8f48e02a35a7fabc
varley44 <javarley44@gmail.com>
2020-04-30 19:32:44 -0400

    Commit for testing

--- reinforcement/valueIterationAgents.py
+++ reinforcement/valueIterationAgents.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -42,9 +42,29 @@ class ValueIterationAgent(ValueEstimationAgent):
         self.discount = discount
         self.iterations = iterations
         self.values = util.Counter() # A Counter is a dict with default 0
+        self.actions = util.Counter()
 
         # Write value iteration code here
         "*** YOUR CODE HERE ***"
+        for state in self.mdp.getStates(): #get all the states
+            self.values[state] = 0.0
+        for i in range(self.iterations):
+            print "Rreeee"
+            states = mdp.getStates()
+            for state in state:
+                if mdp.isTerminal(state):
+                    continue
+                acts = mdp.getPossibleActions(state)
+                maxval = None
+                for act in acts:
+                    val = self.computeQValueFromValues(state,act)
+                    if maxval is None or maxval < val:
+                        maxval = val
+                        self.actions[state] = act
+                self.values[state] = maxval
+
+
+
 
 
     def getValue(self, state):
@@ -60,7 +80,15 @@ class ValueIterationAgent(ValueEstimationAgent):
           value function stored in self.values.
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        pairs = self.mdp.getTransitionStatesAndProbs(state,action)
+        sum = 0
+        for pair in pairs:
+            nstate = pair[0]
+            prob = pair[1]
+            sum += prob * (self.mdp.getReward(state,action,nstate) + (self.discount * self.values[state]))
+        return sum
+
+
 
     def computeActionFromValues(self, state):
         """
@@ -72,7 +100,7 @@ class ValueIterationAgent(ValueEstimationAgent):
           terminal state, you should return None.
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return self.actions[state]
 
     def getPolicy(self, state):
         return self.computeActionFromValues(state)
